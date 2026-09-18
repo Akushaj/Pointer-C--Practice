97c43eafcfebdaf4d45eba823d146ad822397375
Akshaj Kapri <kapriankit1@gmail.com>
2026-09-17 17:13:43 -0700

    Added an append section

--- LinkedList.h
+++ LinkedList.h
@@ -3,6 +3,8 @@
 //
 
 #pragma once
+#include "Node.h"
+
 template<typename T>
 class LinkedList {
 public:
@@ -24,8 +26,22 @@ public:
     void print() {
         Node<T> *temp1 = head;
          while (temp1 != nullptr) {
-             cout << temp1->print()<< endl;
+             temp1->print();
              temp1 = temp1->next; // never move the head pointer cause we can't loose it so we assigne it to a temporary pointer
          }
+    }
+
+    void append(T *value) { // adding to the last of the list
+        Node<T> *newNode = new Node<T>(value); // taking the value and covering it with a Node, calls a node constructor and always point to null cause
+        if (head == nullptr) {
+            head = newNode;
+            size++;
+            return;
+        }
+        Node<T> *temp = head;
+        while (temp->next != nullptr) { // temp ends up one before the last one which is the null pointer and you dont want anything to be at null pointer cause u will lose track of where you came from
+            temp = temp->next;
+        }
+        temp->next = newNode; // the link to null pointer is broken and it will point to newNode
     }
 };
