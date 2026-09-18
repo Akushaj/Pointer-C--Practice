4c6bd544e337ad374b55fc87f2d6ba9400f9c25a
Akshaj Kapri <kapriankit1@gmail.com>
2026-09-17 19:07:34 -0700

    need to add delete at the end

--- LinkedList.h
+++ LinkedList.h
@@ -9,7 +9,7 @@ template<typename T>
 class LinkedList {
 public:
     Node<T> *head;
-    // Node<T> *tail;
+    Node<T> *tail;
     int size;
 
     // We are doing overloading where we can use the same name for constructor but have different values in the parameters
@@ -17,10 +17,12 @@ public:
     LinkedList(T *value) { // have at least one value when creating a linked list
         Node<T> *temp = new Node<T>(value);
         head = temp;
+        tail = temp;
         size = 1;
     }
     LinkedList() { // if you want a empty linkedList constructor
         head = nullptr;
+        tail = nullptr;
         size = 0;
     }
     void print() {
@@ -35,13 +37,18 @@ public:
         Node<T> *newNode = new Node<T>(value); // taking the value and covering it with a Node, calls a node constructor and always point to null cause
         if (head == nullptr) {
             head = newNode;
-            size++;
+            size++; // increase the size of the linkedList
             return;
         }
-        Node<T> *temp = head;
-        while (temp->next != nullptr) { // temp ends up one before the last one which is the null pointer and you dont want anything to be at null pointer cause u will lose track of where you came from
-            temp = temp->next;
-        }
-        temp->next = newNode; // the link to null pointer is broken and it will point to newNode
+        // Node<T> *temp = head;
+        // while (temp->next != nullptr) { // temp ends up one before the last one which is the null pointer and you dont want anything to be at null pointer cause u will lose track of where you came from
+        //     temp = temp->next;
+        // }
+
+        tail->next = newNode; //it becomes O(1) when we add tail but if we only use head and not tail then
+        tail = tail->next; // tail=newnode
+        // the link to null pointer is broken and it will point to newNode
+        size++; // increase the size of the linkedlist
     }
+    // add delete at the end hmmm
 };
