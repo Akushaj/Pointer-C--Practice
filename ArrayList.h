4bba1bd616c2e3ce5b7f4c3d79cf0786ddee2e09
Akshaj Kapri <kapriankit1@gmail.com>
2026-09-22 23:10:50 -0700

    Changed ArrayList , added override to each methods added a new method called ArrayList() which deletes every pointer still in the array

--- ArrayList.h
+++ ArrayList.h
@@ -5,15 +5,16 @@
 # pragma once
 
 #include <iostream>
+#include "List.h"
 using namespace std;
 
 template <typename T>
-class ArrayList {
+class ArrayList : public List<T> {
 public:
     ArrayList():size(0) {
 
     }
-    void add(T item) { // add from the front
+    /*void add(T item) { // add from the front
         if (size >= CAPACITY) {
             cout<< "Array is full" << endl;
             return;
@@ -76,5 +77,38 @@ public:
 private:
     static const int CAPACITY = 20;
     T data[CAPACITY];
-    int size;
+    int size;*/ // The old code
+    void addFront(T* value) override {
+        if (size_ >= CAPACITY) {
+            std:: cout << "ArrayList is Full" << std::endl;
+            return;
+        }
+        for (int i = size_; i > 0; --i) {
+            data_[i] = data_[i - 1 ];
+        }
+        data_[0] = value;
+        ++size_;
+    }
+    bool search(T* value) const override {
+        for (int i =0; i < size_; ++i) {
+            if (*data_[i] == *value) return true;
+        }
+        return false;
+    }
+    void print() const override {
+        for (int i = 0; i < size_; ++i) {
+            std::cout << *data_[i] << ",";
+        }
+        std::cout << std::endl;
+    }
+    ArrayList() override {
+        for (int i = 0; i < size_ ; ++i) {
+            delete data_[i];
+        }
+    }
+
+private:
+    static const int CAPACITY = 20;
+    T* data_[CAPACITY];
+    int size_;
 };
